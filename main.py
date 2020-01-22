e3f155fcef73a37d2707973793247b4ac956f599
almex-fukuda-oji <59679708+almex-fukuda-oji@users.noreply.github.com>
2020-01-22 16:00:06 +0900

    make

--- main.py
+++ main.py
@@ -51,6 +51,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    word = event.message.text
+    result = "あああ"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="buriburi")
