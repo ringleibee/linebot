083ec0e66133fa769dc9f4cd1d9658cd0eee878f
almex-fukuda-oji <59679708+almex-fukuda-oji@users.noreply.github.com>
2020-01-22 15:55:09 +0900

    aaa

--- main.py
+++ main.py
@@ -43,11 +43,17 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=aaaa)
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TextSendMessage(text=event.message.text))
+
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=buriburi)
 
 if __name__ == "__main__":
 #    app.run()
