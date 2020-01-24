4a4cbd840e37653f85db1c5718a523e3ddd667fe
almex-fukuda-oji <59679708+almex-fukuda-oji@users.noreply.github.com>
2020-01-24 14:03:50 +0900

    aaa

--- main.py
+++ main.py
@@ -80,6 +80,9 @@ def handle_message(event):
   
     line_bot_api.reply_message(
         event.reply_token,confirm_template_message
+        event.reply_token,confirm_template_message
+        event.reply_token,confirm_template_message
+        event.reply_token,confirm_template_message
     )
 #  word = event.message.text
 
