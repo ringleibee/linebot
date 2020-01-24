fe16b1296ccfd3531401113b3f3409b17e8790f6
almex-fukuda-oji <59679708+almex-fukuda-oji@users.noreply.github.com>
2020-01-24 13:53:33 +0900

    yeaa

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def handle_message(event):
             ),
             MessageAction(
                 label='のー',
-                text='のー')
+                text='おっぱい')
         ]
         )
     )
